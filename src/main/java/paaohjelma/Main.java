78a24ff25128b79243531f69031afeb34f5dfa03
János Pap <murmurian@gmail.com>
2021-03-11 19:46:30 +0200

    checkstyle korjattu

--- src/main/java/paaohjelma/Main.java
+++ src/main/java/paaohjelma/Main.java
@@ -15,20 +15,5 @@ public class Main {
         mehua.lisaaVarastoon(-666.0);
         double saatiin = olutta.otaVarastosta(1000.0);
         saatiin = mehua.otaVarastosta(-32.9);
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
-        System.out.println("RIKOTAAN CHECKSTYLE!!!");
     }
 }
